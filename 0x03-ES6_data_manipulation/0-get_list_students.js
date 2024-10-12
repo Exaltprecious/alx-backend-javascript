4e42fc523c8c32d9d0b33b411d77f0027b5b32a3
Exaltprecious <peculiarchristj1234@gmail.com>
2024-10-12 19:30:00 +0100

    A function named getListStudents that returns an array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 /**
  * Retrieves a list of students.
  * @author Eneh Chinelo Peculiar <https://github.com/Mikelonia>
- * @returns {{id: Number, firstName: String, location: String}[]}
+ * @returns {{id: number, firstName: string, location: string}[]} An array of student objects.
  */
 export default function getListStudents() {
   return [
-	  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
