e00e98e24c636408ce0d1ef67698f611b81b6e0f
Exaltprecious <peculiarchristj1234@gmail.com>
2024-11-30 01:00:52 +0100

    A

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Eneh Chinelo Peculiar <https://github.com/Exaltprecious>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
